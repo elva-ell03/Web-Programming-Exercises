0eab910bfa5d7669fed52415d0c6bb9ac20b871c
elva-ell03 <80907562+elva-ell03@users.noreply.github.com>
2021-03-23 13:08:09 +0700

    Praktikum 02

---
+++ prak020201.php
@@ -0,0 +1,8 @@
+<?php
+$baris = 4;
+$kolom = 5;
+echo "<table border=1>";
+for($i=1;$i<=$baris;$i++){echo "<tr>";for ($j=1;$j<=$kolom;$j++){echo "<td>";echo "Hello";echo "</td>";}
+echo "</tr>";}
+echo "</table>";
+?>
