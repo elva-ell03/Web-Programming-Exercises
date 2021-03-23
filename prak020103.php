0cea2a7d7caeccaa94b3021362d2a51c09af9151
elva-ell03 <80907562+elva-ell03@users.noreply.github.com>
2021-03-23 13:05:22 +0700

    Praktikum 02

---
+++ prak020103.php
@@ -0,0 +1,9 @@
+<html>
+	<head>Headings</head><br>
+	<body>
+	<?php
+		for($i=1;$i<=6;$i++){if($i%2==1){echo "<font color='black'<h".$i.">Heading ".$i."</h".$i."></font><br>";}
+		else{echo "<font color='red'<h".$i.">Heading ".$i."</h".$i."></font><br>";}}
+	?>
+	</body>
+</html>
